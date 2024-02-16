63a173363ce7a304881922d46f93bf495fc1be4c
being <144606524+entika-ani@users.noreply.github.com>
2024-02-17 02:38:27 +0400

    war_princeess

---
+++ app/src/main/java/com/example/trimino/splashSqreen.java
@@ -0,0 +1,32 @@
+package com.example.trimino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class splashSqreen extends AppCompatActivity {
+    TextView appname;
+    LottieAnimationView lottie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_sqreen);
+
+        appname = findViewById(R.id.appname);
+        
+        appname.animate().translationY(-1400).setDuration(2700).setStartDelay(0);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        }, 5000);
+    }
+}
--- app/src/main/java/com/example/trimino/table.java
+++ app/src/main/java/com/example/trimino/table.java
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class First extends AppCompatActivity {
+public class table extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_first);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table);
     }
 }
